9b5133b88826e783d32a1f9588e9e681275b2c94
Benjamin Chiang <chiangbenjamin991@gmail.com>
2022-05-05 18:17:11 -0700

    feat(server): add server configuration options
    Configuration can be set through environment variables or through json
    files in "config/". Configuration files used for local environments
    should be prefixed with "local-" unless the developer intends to push
    new configuration fields to production.
    
    Find more details at https://github.com/node-config/node-config.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 const http = require('http');
 
+const config = require('config');
+
 const { createServer } = require('./server');
 
 const server = http.createServer(createServer());
 
-server.listen(err => {
+const address = config.get('server.address');
+const port = config.get('server.port');
+
+server.listen(port, address, err => {
   if (err) return console.error(err);
 
   const { address, port } = server.address();
