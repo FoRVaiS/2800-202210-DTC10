668511ecbba16c3a57cc0561a8d3cd47ca097937
Benjamin Chiang <chiangbenjamin991@gmail.com>
2022-05-06 14:15:26 -0700

    feat: redirect user to home on successful sign-in

--- public/pages/login/login.js
+++ public/pages/login/login.js
@@ -1,3 +1,21 @@
 (() => {
-  
+  const inputLoginRef = document.querySelector('#in-log-in');
+
+  const usernameRef = document.querySelector("input[name='username']");
+  const passwordRef = document.querySelector("input[name='password']");
+
+  inputLoginRef.onclick = (e) => {
+    e.preventDefault();
+
+    const username = usernameRef.value;
+    const password = passwordRef.value;
+
+    fetch('/login', {
+      method: 'post',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password }),
+    }).then(data => data.json()).then(data => {
+      if (data.success) window.location.href = '/';
+    });
+  }
 })();
