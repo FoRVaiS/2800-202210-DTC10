83cfbd6db7a5ef82f0b0b40cf8398e928725f01b
Benjamin Chiang <chiangbenjamin991@gmail.com>
2022-05-06 02:22:21 -0700

    feat(route): restrict fetchAccounts route to administrators

--- src/controllers/fetchAccounts.controller.js
+++ src/controllers/fetchAccounts.controller.js
@@ -1,6 +1,13 @@
 const data = require('../__database__');
 
-const fetchAccountsPostController = (_, res) => {
+const fetchAccountsPostController = (req, res) => {
+  const { userUid } = req.session;
+
+  // Get the user whose uid matches session.uid
+  const user = data.users.filter(user => user.uid === userUid)[0];
+
+  if (!user?.roles.includes('admin')) return res.status(403).json({ success: false });
+
   res.status(200).json({
     success: true,
     users: data?.users ?? {},
