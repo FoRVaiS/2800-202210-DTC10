b00997cd6f4af39c587b33c3bc3b407f1cc1ca3a
Shey Barpagga <sheybarpagga@gmail.com>
2022-05-11 16:43:33 -0700

    initialized home page panel

---
+++ public/pages/index/index.js
@@ -0,0 +1,31 @@
+const { append } = require("express/lib/response");
+
+document.addEventListener("DOMContentLoaded", () => {
+
+})
+
+function createSalary() {
+
+    let container = document.createElement("div");
+    container.classList.add("salary-box");
+
+    var location = document.createElement("p");
+    location.classList.add("location");
+
+    var title = document.createElement("h3");
+    title.classList.add("job-title");
+
+    var company = document.createElement("p");
+    company.classList.add("company-title");
+
+    var age = document.createElement("p");
+    var gender = document.createElement("p");
+    age.classList.add("small-info");
+    gender.classList.add("small-info");
+
+    let main = document.getElementById("salary-container");
+
+    
+    
+
+}
--- src/__database__.js
+++ src/__database__.js
@@ -5,6 +5,12 @@ module.exports = {
       username: 'user1',
       password: '123',
       roles: ['admin', 'member'],
+      location: 'Vancouver',
+      title: 'Junior Dev',
+      company: 'EA',
+      gender: 'M',
+      age: 19,
+      wage: 2.03
     },
   ],
 };
