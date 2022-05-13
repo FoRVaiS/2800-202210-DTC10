1f78335be67125b67e72dad9ca06caad4d663743
Shey Barpagga <sheybarpagga@gmail.com>
2022-05-13 02:29:45 -0700

    tried implementing report section

--- public/pages/index/index.js
+++ public/pages/index/index.js
@@ -1,12 +1,12 @@
-const { append } = require("express/lib/response");
+// const { append } = require("express/lib/response");
 
 document.addEventListener("DOMContentLoaded", () => {
-
+    createSalary();
 })
 
 function createSalary() {
 
-    let container = document.createElement("div");
+    var container = document.createElement("div");
     container.classList.add("salary-box");
 
     var location = document.createElement("p");
@@ -14,6 +14,8 @@ function createSalary() {
 
     var title = document.createElement("h3");
     title.classList.add("job-title");
+
+    var salary = document.createElement("h3");
 
     var company = document.createElement("p");
     company.classList.add("company-title");
@@ -25,7 +27,24 @@ function createSalary() {
 
     let main = document.getElementById("salary-container");
 
-    
-    
+    fetch("/accounts", {
+        method: "post",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ uid }),
+      }).then(data => {
+          var id = data.uid;
+      })
 
+    var report = document.createElement("a");
+    report.innerHTML = "R";
+    report.onclick = createReport(id);
+    main.appendChild(report);
+}
+
+function createReport(id) {
+    fetch("/reports", {
+        method: "post",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ id }),
+      });
 }
