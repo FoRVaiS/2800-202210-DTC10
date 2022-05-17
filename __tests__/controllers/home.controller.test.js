faa355ea9a2aa0cdd01a7cfcfd7e667f339d04c4
Benjamin Chiang <chiangbenjamin991@gmail.com>
2022-05-17 12:53:31 -0700

    test: create home unit tests

---
+++ __tests__/controllers/home.controller.test.js
@@ -0,0 +1,41 @@
+const HomeController = require("../../src/controllers/home.controller");
+
+const baseReq = {
+  session: {},
+  uid: {},
+};
+
+const fakeRes = {
+  redirect: jest.fn(),
+  render: jest.fn(),
+};
+
+describe('HomeController', () => {
+  describe('renderHomePage', () => {
+    it('should render the page given the user is authenticated', () => {
+      const fakeReq = {
+        ...baseReq,
+        session: {
+          isAuthenticated: true,
+        },
+      };
+
+      HomeController.renderHomePage(fakeReq, fakeRes);
+
+      expect(fakeRes.render).toHaveBeenCalledTimes(1);
+    });
+
+    it('should redirect when the user is a visitor', () => {
+      const fakeReq = {
+        ...baseReq,
+        session: {
+          isAuthenticated: false,
+        },
+      };
+
+      HomeController.renderHomePage(fakeReq, fakeRes);
+
+      expect(fakeRes.redirect).toHaveBeenCalledTimes(1);
+    });
+  });
+});
