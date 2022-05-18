d857cf288f1fe5333f666bcd0face69c829d90c7
Benjamin Chiang <chiangbenjamin991@gmail.com>
2022-05-17 22:10:53 -0700

    refactor: structure api routes to be more consistent and logical

--- src/routes/apiv1.js
+++ src/routes/apiv1.js
@@ -8,11 +8,14 @@ const ReportController = require('../controllers/report.controller');
 
 const router = express.Router();
 
+// Report
 router.get('/report', ReportController.fetchAllReports);
-router.get('/accounts', UserController.fetchAllAccounts);
-router.post('/login', UserController.login);
-router.post('/logout', UserController.logout);
-router.post('/reports', ReportController.submitReport);
-router.post('/register', UserController.register);
+router.post('/report', ReportController.submitReport);
+
+// User
+router.get('/user', UserController.fetchAllAccounts);
+router.post('/user/login', UserController.login);
+router.post('/user/logout', UserController.logout);
+router.post('/user/register', UserController.register);
 
 module.exports = { router };
