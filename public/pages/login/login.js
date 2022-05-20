6d0d48a0f5ef659942592ac73a93fd5c9f4f9d6a
GhilliedFox <richardcfenton@gmail.com>
2022-05-19 21:53:35 -0700

    repairing bad route

--- public/pages/login/login.js
+++ public/pages/login/login.js
@@ -1,5 +1,5 @@
 (() => {
-  const inputLoginRef = document.querySelector('#in-log-in');
+  const inputLoginRef = document.querySelector("#login");
 
   const emailRef = document.querySelector("input[name='email']");
   const passwordRef = document.querySelector("input[name='password']");
@@ -10,19 +10,21 @@
     const email = emailRef.value;
     const password = passwordRef.value;
 
-    fetch('/api/v1/user/login', {
-      method: 'post',
+    fetch("/api/v1/user/login", {
+      method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
-    }).then(data => data.json()).then(data => {
-      if (data.success) return window.location.href = '/';
+    })
+      .then((data) => data.json())
+      .then((data) => {
+        if (data.success) return (window.location.href = "/");
 
-      const noticeRef = document.querySelector('form > .notice');
-      noticeRef.classList.add('notice--alert');
+        const noticeRef = document.querySelector("form > .notice");
+        noticeRef.classList.add("notice--alert");
 
-      const alertText = document.createElement('p');
-      alertText.textContent = data.msg;
-      noticeRef.append(alertText);
-    });
-  }
+        const alertText = document.createElement("p");
+        alertText.textContent = data.msg;
+        noticeRef.append(alertText);
+      });
+  };
 })();
