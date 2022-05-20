fce8def6714071af387c0bd404626ea7a7ef2dfe
Shey Barpagga <sheybarpagga@gmail.com>
2022-05-19 21:49:46 -0700

    nothing changed, commit for checkout

--- public/pages/index/index.js
+++ public/pages/index/index.js
@@ -1,74 +1,55 @@
-// // const { append } = require("express/lib/response");
+// const { append } = require("express/lib/response");
 
-// document.addEventListener("DOMContentLoaded", () => {
-//     createSalary();
-// })
+document.addEventListener("DOMContentLoaded", () => {
+    createSalary();
+})
 
-// function createSalary() {
+function createSalary() {
 
+    var id = localStorage.getItem("id");
+    payRow(id);
+}
 
-//     //Change classes to Bootstrap containers 
-//     var container = document.createElement("div");
-//     container.classList.add("salary-box");
+function payRow(id) {
 
-//     var location = document.createElement("p");
-//     location.classList.add("location");
+    var main = document.getElementById("Pay-Table");
+    var currentUserJob;
 
-//     var title = document.createElement("h3");
-//     title.classList.add("job-title");
+    fetch("/api/v1/salary", {
+        method: "get",
+        headers: { "Content-Type": "application/json" },
+      }).then(data => data.json()).then(data => {
+        var users = data.data;
+        users.forEach(element => {
+            if ((element.userId).localeCompare( id)) {
+                currentUserJob = element.position;
+            }
+        })
+            users.forEach(element => {
+            if(element.position == currentUserJob) {
+                var tr = main.insertRow();
+                tr.setAttribute("id", `${element.userId}`);
+                var company = tr.insertCell();
+                var gender = tr.insertCell();
+                var age = tr.insertCell();
+                var pay = tr.insertCell();
+                company.appendChild(document.createTextNode(`${element.company}`));
+                gender.appendChild(document.createTextNode(`${"temp"}`));
+                age.appendChild(document.createTextNode(`${"temp"}`));
+                pay.appendChild(document.createTextNode(`${element.salary}`));
 
-//     var salary = document.createElement("h3");
+                tr.addEventListener("onclick", createReport(element.userId));
+            }
+        })
+    });
 
-//     var company = document.createElement("p");
-//     company.classList.add("company-title");
+}
 
-//     var age = document.createElement("p");
-//     var gender = document.createElement("p");
-//     age.classList.add("small-info");
-//     gender.classList.add("small-info");
-
-//     let main = document.getElementById("salary-container");
-
-//     fetch("/api/v1/user", {
-//         method: "get",
-//         headers: { "Content-Type": "application/json" },
-//         body: JSON.stringify({__id}),
-//       }).then(data => data.json()).then(data => {
-//             var id = data.uid;    
-//             var report = document.createElement("a");
-//             report.innerHTML = "R";
-//             report.addEventListener("onClick", createReport(id));
-//             main.appendChild(report);
-//       })
-
-// }
 
 // function createReport(id) {
 //     fetch("/api/v1/report/post", {
 //         method: "post",
 //         headers: { "Content-Type": "application/json" },
-//         body: JSON.stringify({ id }),
+//         body: JSON.stringify({id}),
 //       });
 // }
-
-
-document.addEventListener("DOMContentLoaded", () => {
-
-var map = L.map('map');
-map.locate({setView : true});
-var markersLayer = new L.LayerGroup();	//layer contain searched elements
-map.addLayer(markersLayer);
-
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2hleWJhcnBhZ2dhIiwiYSI6ImNsM2M4YXB0NzAxYWszbG9rMTNiN3poNDEifQ.d1_I76W_smy83ImELB7N4w', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'your.mapbox.access.token'
-}).addTo(map);
-
-
-data =[{"loc": [49.250889, -123.004410], "title": "BCIT"}];
-
-})
