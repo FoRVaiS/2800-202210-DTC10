851d91b7ed4453e051c6d17d97fec12b68fe1088
Benjamin Chiang <chiangbenjamin991@gmail.com>
2022-05-25 09:44:07 -0700

    feat: add salary submission form view route

--- src/routes/views.js
+++ src/routes/views.js
@@ -14,5 +14,6 @@ router.get('/register', UserController.renderRegistrationPage);
 router.get('/login', UserController.renderLoginPage);
 router.get('/admin', UserController.renderAdminPage);
 router.get('/search', SearchController.renderSearchPage);
+router.get('/form/submission', FormController.renderSalarySubmissionPage);
 
 module.exports = { router };
