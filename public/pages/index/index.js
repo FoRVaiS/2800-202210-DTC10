9281b23e36ab765421d5aed03a7f5057cb7a40c8
Benjamin Chiang <chiangbenjamin991@gmail.com>
2022-05-25 21:01:35 -0700

    style: format document

--- public/pages/index/index.js
+++ public/pages/index/index.js
@@ -1,6 +1,5 @@
 var userPay;
 (() => {
-
   const userData = [];
   // const userPay = 0;
   // Stores a reference to a function that tracks of the number of clicks in a given timeframe.
@@ -54,15 +53,17 @@ var userPay;
   if (addToSalaryBtn) {
     addToSalaryBtn.onclick = () => window.location.href = '/form/salary';
   }
+
   var salary = []
   var company = []
   var id = localStorage.getItem("id");
   payRow(id, userData);
- 
-  setTimeout(function () {
+
+  setTimeout(function() {
     console.log(userPay)
     userData.sort((a, b) => a.salary - b.salary);
-    for(var x = 0; x < userData.length; x++) {
+
+    for (var x = 0; x < userData.length; x++) {
       salary[x] = userData[x].salary;
       company[x] = userData[x].companyName;
     }
@@ -80,179 +81,168 @@ var userPay;
         }]
       },
       options: {
-        legend: {display: false},
+        legend: {
+          display: false
+        },
       },
       elements: {
         point: {
-          radius : highLight,
+          radius: highLight,
           display: true
         }
       }
     });
   }, 300)
 
-
-  
   searchCompany();
-
-  
 
   document.getElementById("sort").addEventListener("click", () => {
     sortTable();
-  })
-  
-
-
-
+  });
 })();
 
-
-
 function userInfo(data) {
+  var jobBox = document.createElement("div");
+  jobBox.classList.add("d-flex");
+  jobBox.classList.add("align-items-start");
+  jobBox.classList.add("flex-column");
 
-    var jobBox = document.createElement("div");
-    jobBox.classList.add("d-flex");
-    jobBox.classList.add("align-items-start");
-    jobBox.classList.add("flex-column");
+  var userBox = document.getElementById("userInfo");
+  var company = document.createElement("h3");
+  var companyName = document.createElement("small");
+  var positionName = document.createElement("small");
 
-    var userBox = document.getElementById("userInfo");
-    var company = document.createElement("h3");
-    var companyName = document.createElement("small");
-    var positionName = document.createElement("small");
+  companyName.textContent = `${data.company}`;
+  companyName.classList.add("text-muted");
+  positionName.textContent = `${data.position}`;
+  positionName.classList.add("text-muted");
 
-    companyName.textContent = `${data.company}`;
-    companyName.classList.add("text-muted");
-    positionName.textContent = `${data.position}`;
-    positionName.classList.add("text-muted");
+  var position = document.createElement("h4");
+  var pay = document.createElement("h2");
+  var payBox = document.createElement("div");
 
-    var position = document.createElement("h4");
-    var pay = document.createElement("h2");
-    var payBox = document.createElement("div");
+  payBox.classList.add("d-flex");
+  payBox.classList.add("justify-content-center");
+  payBox.appendChild(pay);
 
-    payBox.classList.add("d-flex");
-    payBox.classList.add("justify-content-center");
-    payBox.appendChild(pay);
+  company.textContent = `Company: `;
+  company.appendChild(companyName);
+  position.textContent = `Position: `;
+  position.appendChild(positionName);
+  pay.textContent = `$${data.salary}/hr`;
 
-    company.textContent = `Company: `
-    company.appendChild(companyName);
-    position.textContent = `Position: `
-    position.appendChild(positionName);
-    pay.textContent = `$${data.salary}/hr`
-
-    jobBox.appendChild(company);
-    jobBox.appendChild(position);
-    userBox.appendChild(jobBox);
-    userBox.appendChild(payBox);
+  jobBox.appendChild(company);
+  jobBox.appendChild(position);
+  userBox.appendChild(jobBox);
+  userBox.appendChild(payBox);
 }
 
 function payRow(id, userData) {
+  var main = document.getElementById("Pay-Table");
+  var currentUserJob;
 
-    var main = document.getElementById("Pay-Table");
-    var currentUserJob;
+  fetch("/api/v1/salary")
+    .then(data => data.json())
+    .then(data => {
+      var users = data.data;
+      
+      users.forEach(element => {
+        if (element.userId === id) {
+          currentUserJob = element.position;
+          userPay = element.salary;
+          userInfo(element);
+        }
+      });
 
+      users.forEach(element => {
+        if (element.position == currentUserJob) {
+          fetch(`/api/v1/user/id/${element.userId}`)
+            .then(userPersonal => userPersonal.json())
+            .then(userPersonal => {
+                var tr = main.insertRow();
+                tr.setAttribute("id", `${element.postId}`);
+                var company = tr.insertCell();
+                var location = tr.insertCell();
+                var gender = tr.insertCell();
+                var age = tr.insertCell();
+                var pay = tr.insertCell();
+                var report = tr.insertCell();
 
-    fetch("/api/v1/salary", {
-        method: "get",
-        headers: { "Content-Type": "application/json" },
-      }).then(data => data.json()).then(data => {
-        var users = data.data;
-        users.forEach(element => {        
+                userData.push({
+                  salary: (element.salary),
+                  companyName: (element.company)
+                });
 
-            if (element.userId === id) {
-                currentUserJob = element.position;
-                userPay = element.salary;
-                userInfo(element);
-            }
-        })
-            users.forEach(element => {
-            if(element.position == currentUserJob) {
-                fetch(`/api/v1/user/id/${element.userId}`, {
-                    method: "get",
-                    headers: { "Content-Type": "application/json" },
-                }).then(userPersonal => userPersonal.json()).then(userPersonal => {
-                    var tr = main.insertRow();
-                    tr.setAttribute("id", `${element.postId}`);
-                    var company = tr.insertCell();
-                    var location = tr.insertCell();
-                    var gender = tr.insertCell();
-                    var age = tr.insertCell();
-                    var pay = tr.insertCell();
-                    var report = tr.insertCell();
-                    
-                    userData.push({salary: (element.salary), companyName: (element.company)});
+                var reportButton = document.createElement("button");
+                reportButton.type = "button";
+                reportButton.classList.add("btn");
+                reportButton.classList.add("btn-danger");
+                reportButton.classList.add("btn-sm");
 
-                    var reportButton = document.createElement("button");
-                    reportButton.type = "button";
-                    reportButton.classList.add("btn");
-                    reportButton.classList.add("btn-danger");
-                    reportButton.classList.add("btn-sm");
-
-                    reportButton.addEventListener("click", () => {
-                        createReport(element.postId);
-                    })
-                    reportButton.innerHTML = "Report";
-                    company.appendChild(document.createTextNode(`${element.company}`));
-                    location.appendChild(document.createTextNode(`${element.location}`));
-                    gender.appendChild(document.createTextNode(`${userPersonal.data.gender}`));
-                    age.appendChild(document.createTextNode(`${userPersonal.data.age}`));
-                    pay.appendChild(document.createTextNode(`${element.salary}`));
-                    report.appendChild(reportButton);
+                reportButton.addEventListener("click", () => {
+                  createReport(element.postId);
                 })
-
-
-               
-            }
-        })
+                reportButton.innerHTML = "Report";
+                company.appendChild(document.createTextNode(`${element.company}`));
+                location.appendChild(document.createTextNode(`${element.location}`));
+                gender.appendChild(document.createTextNode(`${userPersonal.data.gender}`));
+                age.appendChild(document.createTextNode(`${userPersonal.data.age}`));
+                pay.appendChild(document.createTextNode(`${element.salary}`));
+                report.appendChild(reportButton);
+              });
+        }
+      })
     });
-    userData.sort((a, b) => a.salary - b.salary);
-    return userPay;
+  userData.sort((a, b) => a.salary - b.salary);
+  return userPay;
 }
 
-function searchCompany() {    
-    var searchButton = document.getElementById("search-input");
-    searchButton.addEventListener("click", () => {
-        var input = document.getElementById("tags").value;
-        localStorage.setItem("search", `${input}`);
-        return window.location.href = "/search";
-    });
+function searchCompany() {
+  var searchButton = document.getElementById("search-input");
+  searchButton.addEventListener("click", () => {
+    var input = document.getElementById("tags").value;
+    localStorage.setItem("search", `${input}`);
+    return window.location.href = "/search";
+  });
 }
 
-$( function() {
-    var companies = [
-        "McDonald's",
-        "No Frills",
-        "Real Canadian Superstore",
-        "Winners",
-        "Shopper's Drug Mart"
-    ];
-    $( "#tags" ).autocomplete({
-      source: companies
-    });
-  } );
-
-
+$(function() {
+  var companies = [
+    "McDonald's",
+    "No Frills",
+    "Real Canadian Superstore",
+    "Winners",
+    "Shopper's Drug Mart"
+  ];
+  $("#tags").autocomplete({
+    source: companies
+  });
+});
 
 function createReport(postId) {
   document.getElementById(postId).remove();
   $('#report-modal').modal('show')
-    fetch("/api/v1/report/post", {
-        method: "post",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ postId }),
-      });
+  fetch("/api/v1/report/post", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      postId
+    }),
+  });
 }
-
 
 function sortTable() {
   myTable = document.getElementById("Pay-Table");
   swap = true;
   rows = myTable.rows;
-  while(swap) {
+  while (swap) {
     swap = false;
-    for(var x = 1; x < (rows.length - 1); x++) {
+    for (var x = 1; x < (rows.length - 1); x++) {
       curRow = rows[x].getElementsByTagName("td")[4];
       nextRow = rows[(x + 1)].getElementsByTagName("td")[4];
-      if(Number(curRow.innerHTML) > Number(nextRow.innerHTML)) {
+      if (Number(curRow.innerHTML) > Number(nextRow.innerHTML)) {
         rows[x].parentNode.insertBefore(rows[x + 1], rows[x]);
         swap = true;
         break;
@@ -261,9 +251,8 @@ function sortTable() {
   }
 }
 
-
 function highLight(context) {
   let index = context.dataIndex;
-  let value = context.dataset.data[ index ];
+  let value = context.dataset.data[index];
   return value == userPay ? 10 : 2;
 }
