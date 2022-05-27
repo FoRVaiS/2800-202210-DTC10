1929ef0e8bca312936befc16474303c351a3debb
Shey Barpagga <sheybarpagga@gmail.com>
2022-05-26 19:18:15 -0700

    added route for 404

--- src/controllers/aboutus.controller.js
+++ src/controllers/aboutus.controller.js
@@ -1,7 +1,7 @@
 const renderAboutUsPage = (req, res) => {
     if (!req.session.isAuthenticated) return res.redirect('/login');
   
-    res.render("pages/error/error.ejs");
+    res.render("pages/about us/aboutus.ejs");
   };
   
   module.exports = { renderAboutUsPage };
