99423219556c8dbff1d8ea9f9928829551e9f627
GhilliedFox <richardcfenton@gmail.com>
2022-05-26 22:06:25 -0700

    commiting due to some weird error

--- public/pages/salary-form/salary-form.js
+++ public/pages/salary-form/salary-form.js
@@ -4,8 +4,8 @@
 
     const response = await fetch(url, {
       headers: {
-        'Content-Type': 'application/json',
-        ...headers
+        "Content-Type": "application/json",
+        ...headers,
       },
       body: JSON.stringify(body),
       ...fetchOpts,
@@ -14,26 +14,37 @@
     return response.json();
   };
 
-  const redirectToPathFromRoot = path => {
-    console.log([(new URL(window.location.href)).origin, path.replace('/', '')].join('/'));
-    return window.location.href = [(new URL(window.location.href)).origin, path.replace('/', '')].join('/');
-  }
+  const redirectToPathFromRoot = (path) => {
+    console.log(
+      [new URL(window.location.href).origin, path.replace("/", "")].join("/")
+    );
+    return (window.location.href = [
+      new URL(window.location.href).origin,
+      path.replace("/", ""),
+    ].join("/"));
+  };
 
   const sendSalarySubmission = async () => {
-    const titleField = document.querySelector('#inputTitle');
-    const addressField = document.querySelector('#inputAddress');
-    const wageField = document.querySelector('#inputWage');
-    const ageField = document.querySelector('#inputAge');
-    const genderField = document.querySelector('#inlineFormCustomSelect');
+    const titleField = document.querySelector("#inputTitle");
+    const addressField = document.querySelector("#inputAddress");
+    const wageField = document.querySelector("#inputWage");
+    const ageField = document.querySelector("#inputAge");
+    const genderField = document.querySelector("#inlineFormCustomSelect");
 
-    if (!titleField) console.warn('Could not find the job title field');
-    if (!addressField) console.warn('Could not find the address field');
-    if (!wageField) console.warn('Could not find the wage field');
-    if (!ageField) console.warn('Could not find the age field');
-    if (!genderField) console.warn('Could not find the gender field');
+    if (!titleField) console.warn("Could not find the job title field");
+    if (!addressField) console.warn("Could not find the address field");
+    if (!wageField) console.warn("Could not find the wage field");
+    if (!ageField) console.warn("Could not find the age field");
+    if (!genderField) console.warn("Could not find the gender field");
 
-    if (!titleField || !addressField || !wageField || !ageField || !genderField) {
-      console.warn('Please resolve the warnings above');
+    if (
+      !titleField ||
+      !addressField ||
+      !wageField ||
+      !ageField ||
+      !genderField
+    ) {
+      console.warn("Please resolve the warnings above");
       return null;
     }
 
@@ -43,46 +54,48 @@
     const age = Number(ageField.value);
     const gender = genderField.value.trim();
 
-    const { success } = await fetchJson('/api/v1/salary/submit', {
-      method: 'post',
-      body: { 
-        position: title, 
+    const { success } = await fetchJson("/api/v1/salary/submit", {
+      method: "post",
+      body: {
+        position: title,
         location: address,
         salary: wage,
         age,
         gender,
-      }
+      },
     });
 
-    const redirectUrl = (new URL(window.location.href)).searchParams.get('redirectUrl');
+    const redirectUrl = new URL(window.location.href).searchParams.get(
+      "redirectUrl"
+    );
 
     if (!success) {
-      console.warn('Form submission failed');
+      console.warn("Form submission failed");
       return null;
     }
 
-    redirectToPathFromRoot(redirectUrl || '/');
-  }
+    redirectToPathFromRoot(redirectUrl || "/");
+  };
 
-  const { data: salaries } = await fetchJson('/api/v1/salary', {});
+  const { data: salaries } = await fetchJson("/api/v1/salary", {});
 
-  const locations = Array.from(new Set(
-    salaries.map(salary => salary.location)
-  ));
+  const locations = Array.from(
+    new Set(salaries.map((salary) => salary.location))
+  );
 
-  $('#inputAddress').autocomplete({
-    source: locations
+  $("#inputAddress").autocomplete({
+    source: locations,
   });
 
   const submitBtn = document.querySelector('button[type="submit"]');
 
   if (submitBtn) {
-    submitBtn.onclick = e => {
+    submitBtn.onclick = (e) => {
       e.preventDefault();
 
       sendSalarySubmission();
     };
   } else {
-    console.warn('Could not find submit button.');
+    console.warn("Could not find submit button.");
   }
 })();
