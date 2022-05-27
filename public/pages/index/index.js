263a1f3bed5f6cbd63eadc52c3196f07fd5e5991
Benjamin Chiang <chiangbenjamin991@gmail.com>
2022-05-26 21:52:09 -0700

    refactor: move admin button logic into a global js file

--- public/pages/index/index.js
+++ public/pages/index/index.js
@@ -1,20 +1,5 @@
 var userPay;
 
-const fetchJson = async (url, opts = {}) => {
-  const { body, headers, ...fetchOpts } = opts;
-
-  const response = await fetch(url, {
-    headers: {
-      'Content-Type': 'application/json',
-      ...headers
-    },
-    body: JSON.stringify(body),
-    ...fetchOpts,
-  });
-
-  return response.json();
-};
-
 (async () => {
   const userData = [];
   // const userPay = 0;
@@ -113,20 +98,6 @@ const fetchJson = async (url, opts = {}) => {
   document.getElementById("sort").addEventListener("click", () => {
     sortTable();
   });
-
-  const adminDashboardBtn = document.querySelector('#admin-input');
-
-  if (adminDashboardBtn) {
-    const { success, data: user } = await fetchJson(`/api/v1/user/id/${id}`);
-
-    if (!success) {
-      console.warn('User is not logged in or could not be found');
-    } else if (user && user.roles && user.roles.includes('admin')) {
-      adminDashboardBtn.onclick = () => window.location.href = '/admin';
-    } else {
-      adminDashboardBtn.remove();
-    }
-  }
 })();
 
 function userInfo(data) {
